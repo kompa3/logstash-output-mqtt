7d6dbf81b31f6f0eccdd18ab1d70436679b59f29
matgnt <matgnt@gmail.com>
2016-10-17 08:30:03 +0200

    Replace variable in "topic" settings (#2)
    * Fix test case to work in other timezones than UTC
    
    Since logstash converts the given timestamp to UTC, this part
    in the "expected message" depends on the timezone where the test
    case is executed. With calculating this, the code is now timezone
    independent.
    
    * Add test case to check variable topic settings
    
    In MQTT it makes sense to have parts of the topics depending on
    parts of an event, e.g. an identifier of the sender of the message.
    
    * Replace variables in "topic" settings
    
    New feature:
    
    mqtt {
      ...
      topic => "data/%{myfield}"
    }

--- spec/outputs/mqtt_spec.rb
+++ spec/outputs/mqtt_spec.rb
@@ -58,3 +58,40 @@ describe LogStash::Outputs::MQTT do
     end
   end
 end
+
+describe "Test subtopic:" do
+  # Define a sample event and its JSON encoded form
+  datestring = "2016-01-01"
+  tstamp = Time.parse(datestring)
+  tstamp_utc = tstamp.utc.iso8601(3)
+  let(:sample_event) { LogStash::Event.new("message" => "test message", "@timestamp" => datestring, "subtopic" => "mysubtopic") }
+  encoded_event = "{\"message\":\"test message\",\"@timestamp\":\"" + tstamp_utc + "\",\"subtopic\":\"mysubtopic\",\"@version\":\"1\"}"
+
+  settings = {
+    "host" => "test.mosquitto.org",
+    "topic" => "hello/%{subtopic}"
+  }
+  let(:output) { LogStash::Outputs::MQTT.new(settings) }
+  let(:stub_client) { Object.new }
+
+  before do
+    output.register
+  end
+
+  describe "receive message on a subtopic based on a field of an event" do
+    it "connects and publishes" do
+
+      expect(MQTT::Client).to receive(:connect).with(
+        :host => "test.mosquitto.org",
+        :port => 8883
+      ) do |*args, &block|
+        block.call(stub_client)
+      end
+
+      expect(stub_client).to receive(:publish).with("hello/mysubtopic", encoded_event, false, 0)
+
+      output.receive(sample_event)
+    end
+  end
+
+end
